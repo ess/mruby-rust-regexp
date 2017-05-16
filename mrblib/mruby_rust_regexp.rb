90415c99923cbfb023f5c7f4ba1ac2bd6bb70e06
Dennis Walters <dwalters@engineyard.com>
2017-05-16 03:31:03 -0400

    whatever

--- mrblib/mruby_rust_regexp.rb
+++ mrblib/mruby_rust_regexp.rb
@@ -1,4 +1,31 @@
-module Rust
-  class Regexp
+class RustRegexp
+  @memo = {}
+
+  attr_reader :source
+
+  def self.compile(*args)
+    as = args.to_s
+    unless @memo.key? as
+      @memo[as] = self.new(*args)
+    end
+    @memo[as]
   end
+
+  def initialize(pattern, option = nil)
+    @source = pattern
+    @option = 0
+    @ignore_case = option.include? 'i'
+    @multi_line = option.include? 'm'
+  end
+
+  def match(string, position = nil)
+    matches = self.class.match(string)
+
+    return nil if matches.empty?
+
+    matches
+  end
+end
+
+class RustMatchData
 end
---
+++ rust/target/debug/libmruby_rust_regexp.d
@@ -0,0 +1,1 @@
+/Users/denniswalters/projects/internal/mruby-rust-regexp/rust/target/debug/libmruby_rust_regexp.a: /Users/denniswalters/projects/internal/mruby-rust-regexp/rust/src/lib.rs
