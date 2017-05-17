96df062bf28262be064107f24eb26b2cd4f171ee
Dennis Walters <dwalters@engineyard.com>
2017-05-17 15:02:49 -0400

    cleanup

--- rust/target/debug/libmruby_rust_regexp.d
+++
@@ -1,1 +0,0 @@
-/Users/denniswalters/projects/internal/mruby-rust-regexp/rust/target/debug/libmruby_rust_regexp.a: /Users/denniswalters/projects/internal/mruby-rust-regexp/rust/src/lib.rs
--- rust/target/debug/librugexp.d
+++
@@ -1,1 +0,0 @@
-/Users/denniswalters/projects/internal/mruby-rust-regexp/rust/target/debug/librugexp.a: /Users/denniswalters/projects/internal/mruby-rust-regexp/rust/src/lib.rs
